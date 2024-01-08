041a41f0ca35718264ccb9115b16dd8483330a3d
Patrick O'Shea <p14oshea@gmail.com>
2024-01-08 20:58:06 +0000

    Remove TODO from list

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 require("pos.core")
 
 -- TODO: get tmux to diplay git stuff from nvim project location
--- TODO: find out more about kitty plugin
 -- TODO: better LSP key bindings, and code action shit
 -- TODO: DAP
 -- TODO: Test out neogit
