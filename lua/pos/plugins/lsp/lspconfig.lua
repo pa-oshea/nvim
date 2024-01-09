4641725f1c94b156a21799eb452f586eff35432e
Patrick O'Shea <p14oshea@gmail.com>
2024-01-09 14:11:59 +0000

    Update lua LSP

--- lua/pos/plugins/lsp/lspconfig.lua
+++ lua/pos/plugins/lsp/lspconfig.lua
@@ -58,9 +58,17 @@ return {
 			capabilities = capabilities,
 			settings = {
 				Lua = {
+					runtime = {
+						version = 'LuaJIT'
+					},
 					diagnostics = {
 						globals = { "vim" },
 					},
+					workspace = {
+						library = {
+							vim.env.VIMRUNTIME,
+						}
+					}
 				},
 			},
 		})
