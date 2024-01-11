df9e0449df6f3e1223f6046dea9777f3af5fec03
Patrick O'Shea <p14oshea@gmail.com>
2024-01-11 18:53:21 +0000

    Update lsp configuration

--- lua/pos/plugins/lsp/lspconfig.lua
+++ lua/pos/plugins/lsp/lspconfig.lua
@@ -2,13 +2,58 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
+		{ "folke/neodev.nvim", opts = {} },
 		"hrsh7th/cmp-nvim-lsp",
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+	},
+	opts = {
+		-- options for vim.diagnostic.config()
+		diagnostics = {
+			underline = false,
+			update_in_insert = false,
+			virtual_text = {
+				spacing = 4,
+				source = "if_many",
+				prefix = "●",
+				-- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+				-- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+				-- prefix = "icons",
+			},
+			severity_sort = true,
+		},
+		-- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+		-- Be aware that you also will need to properly configure your LSP server to
+		-- provide the inlay hints.
+		inlay_hints = {
+			enabled = false,
+		},
+		-- add any global capabilities here
+		capabilities = {},
+		-- options for vim.lsp.buf.format
+		-- `bufnr` and `filter` is handled by the LazyVim formatter,
+		-- but can be also overridden when specified
+		format = {
+			formatting_options = nil,
+			timeout_ms = nil,
+		},
+		-- LSP Server Settings
+		---@type lspconfig.options
+		servers = {
+			lua_ls = {
+				-- mason = false, -- set to false if you don't want this server to be installed with mason
+				-- Use this to add any additional keymaps
+				-- for specific lsp servers
+				---@type LazyKeysSpec[]
+				-- keys = {},
+				settings = {
+					Lua = {},
+				},
+			},
+		},
 	},
 	config = function()
-		local lspconfig = require("lspconfig")
 		local lspui = require("lspconfig.ui.windows")
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-		local capabilities = cmp_nvim_lsp.default_capabilities()
 
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
@@ -16,74 +61,17 @@ return {
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
+		vim.diagnostic.config(vim.deepcopy({
+			underline = true,
+			update_in_insert = false,
+			virtual_text = {
+				source = "if_many",
+				prefix = "●",
+			},
+			severity_sort = true,
+		}))
 		--LspInfo Borders
 		lspui.default_options.border = "double"
-
-		-- Managing language servers individually
-		-- tsserver
-		lspconfig.tsserver.setup({
-			capabilities = capabilities,
-		})
-		-- rust_analyzer
-		-- lspconfig.rust_analyzer.setup({
-		-- 	capabilities = capabilities,
-		-- 	-- Server-specific settings. See `:help lspconfig-setup`
-		-- 	settings = {
-		-- 		["rust-analyzer"] = {},
-		-- 	},
-		-- })
-		lspconfig.gopls.setup({
-			settings = {
-				gopls = {
-					analyses = {
-						unusedparams = true,
-					},
-					staticcheck = true,
-					gofumpt = true,
-				},
-			},
-		})
-		-- html
-		lspconfig.html.setup({
-			capabilities = capabilities,
-		})
-		-- configure emmet language server
-		lspconfig.emmet_ls.setup({
-			capabilities = capabilities,
-			filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-		})
-
-		-- Lua LS
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					runtime = {
-						version = "LuaJIT",
-					},
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						library = {
-							vim.env.VIMRUNTIME,
-						},
-					},
-				},
-			},
-		})
-
-		-- CSS LS
-		lspconfig.cssls.setup({
-			capabilities = capabilities,
-		})
-
-		-- Tailwind
-		-- Support for tailwind auto completion
-		-- install the tailwind server : "sudo npm install -g @tailwindcss/language-server"
-		lspconfig.tailwindcss.setup({
-			capabilities = capabilities,
-		})
 
 		-- Use LspAttach autocommand to only map the following keys
 		-- after the language server attaches to the current buffer
@@ -101,11 +89,12 @@ return {
 				vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
 				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 				vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-				vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-				vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-				vim.keymap.set("n", "<space>wl", function()
-					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-				end, opts)
+				-- TODO: Find out more about these
+				-- vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+				-- vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+				-- vim.keymap.set("n", "<space>wl", function()
+				-- 	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+				-- end, opts)
 				vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
 				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 			end,
--- lua/pos/plugins/lsp/mason.lua
+++ lua/pos/plugins/lsp/mason.lua
@@ -1,65 +1,247 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-
-	config = function()
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+					border = "double",
+					width = 0.8,
+					height = 0.8,
 				},
-				border = "double",
-				width = 0.8,
-				height = 0.8,
-			},
-		})
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
+			local wk = require("which-key")
+			local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- mason-lspconfig
-		mason_lspconfig.setup({
-			ensure_installed = {
-				"lua_ls",
-				"cssls",
-				"marksman",
-			},
-			-- auto installation
-			automatic_installation = true,
-		})
+			local register_keys = function(keys)
+				local opts = {
+					mode = "n", -- NORMAL mode
+					prefix = "<leader>",
+					buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+					silent = true, -- use `silent` when creating keymaps
+					noremap = true, -- use `noremap` when creating keymaps
+					nowait = true, -- use `nowait` when creating keymaps
+				}
+				wk.register(keys, opts)
+			end
+			require("mason-lspconfig").setup({
+				-- auto installation
+				automatic_installation = true,
+			})
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
 
-		-- mason-tool-installer
-		mason_tool_installer.setup({
-			ensure_installed = {
-				-- you can turn off/on auto_update per tool
-				{ "bash-language-server" },
-				{ "lua-language-server" },
-				{ "vim-language-server" },
-				{ "gopls" },
-				{ "stylua" },
-				{ "editorconfig-checker" },
-				{ "html-lsp" },
-				{ "emmet-ls" },
-				{ "css-lsp" },
-				{ "json-lsp" },
-				{ "prettier" },
-				{ "typescript-language-server" },
-				{ "js-debug-adapter" },
-				{ "node-debug2-adapter" },
-				{ "eslint_d" },
-				{ "eslint-lsp" },
-				{ "tailwindcss-language-server" },
-			},
+				--- go lsp
+				["gopls"] = function()
+					lspconfig.gopls.setup({
+						capabilities = capabilities,
+						on_attach = function(client, _)
+							if client.name == "gopls" then
+								if not client.server_capabilities.semanticTokensProvider then
+									local semantic = client.config.capabilities.textDocument.semanticTokens
+									client.server_capabilities.semanticTokensProvider = {
+										full = true,
+										legend = {
+											tokenTypes = semantic.tokenTypes,
+											tokenModifiers = semantic.tokenModifiers,
+										},
+										range = true,
+									}
+								end
 
-			auto_update = true,
-			run_on_start = true,
-			start_delay = 3000, -- 3 second delay
-			debounce_hours = 5, -- at least 5 hours between attempts to install/update
-		})
-	end,
+								register_keys({
+									c = {
+										name = "Go",
+										i = { "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies" },
+										f = { "<cmd>GoMod tidy<cr>", "Tidy" },
+										a = { "<cmd>GoTestAdd<Cr>", "Add Test" },
+										A = { "<cmd>GoTestsAll<Cr>", "Add All Tests" },
+										e = { "<cmd>GoTestsExp<Cr>", "Add Exported Tests" },
+										g = { "<cmd>GoGenerate<Cr>", "Go Generate" },
+										G = { "<cmd>GoGenerate %<Cr>", "Go Generate File" },
+										c = { "<cmd>GoCmt<Cr>", "Generate Comment" },
+										t = { "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test" },
+									},
+								})
+							end
+						end,
+						settings = {
+							gopls = {
+								gofumpt = true,
+								codelenses = {
+									gc_details = false,
+									generate = true,
+									regenerate_cgo = true,
+									run_govulncheck = true,
+									test = true,
+									tidy = true,
+									upgrade_dependency = true,
+									vendor = true,
+								},
+								hints = {
+									assignVariableTypes = true,
+									compositeLiteralFields = true,
+									compositeLiteralTypes = true,
+									constantValues = true,
+									functionTypeParameters = true,
+									parameterNames = true,
+									rangeVariableTypes = true,
+								},
+								analyses = {
+									fieldalignment = true,
+									nilness = true,
+									unusedparams = true,
+									unusedwrite = true,
+									useany = true,
+								},
+								usePlaceholders = true,
+								completeUnimported = true,
+								staticcheck = true,
+								directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+								semanticTokens = true,
+							},
+						},
+					})
+				end,
+
+				-- typescript / javascript
+				["tsserver"] = function()
+					lspconfig.tsserver.setup({
+						capabilities = capabilities,
+						on_attach = function()
+							register_keys({
+								c = {
+									name = "Typescript",
+									o = {
+										function()
+											vim.lsp.buf.code_action({
+												apply = true,
+												context = {
+													only = { "source.organizeImports.ts" },
+													diagnostics = {},
+												},
+											})
+										end,
+										"Organize Imports",
+									},
+									r = {
+										function()
+											vim.lsp.buf.code_action({
+												apply = true,
+												context = {
+													only = { "source.removeUnused.ts" },
+													diagnostics = {},
+												},
+											})
+										end,
+										"Remove Unused Imports",
+									},
+								},
+							})
+						end,
+						settings = {
+							typescript = {
+								format = {
+									indentSize = vim.o.shiftwidth,
+									convertTabsToSpaces = vim.o.expandtab,
+									tabSize = vim.o.tabstop,
+								},
+							},
+							javascript = {
+								format = {
+									indentSize = vim.o.shiftwidth,
+									convertTabsToSpaces = vim.o.expandtab,
+									tabSize = vim.o.tabstop,
+								},
+							},
+							completions = {
+								completeFunctionCalls = true,
+							},
+						},
+					})
+				end,
+
+				-- lua lsp
+				["lua_ls"] = function()
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								completion = {
+									callSnippet = "Replace",
+								},
+								runtime = {
+									version = "LuaJIT",
+								},
+								diagnostics = {
+									globals = { "vim" },
+								},
+								workspace = {
+									checkThirdParty = false,
+									library = {
+										vim.env.VIMRUNTIME,
+									},
+								},
+							},
+						},
+					})
+				end,
+			})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					-- you can turn off/on auto_update per tool
+					{ "bashls" },
+					{ "cssls" },
+					{ "delve" },
+					{ "eslint_d" },
+					{ "eslint-lsp" },
+					{ "gopls" },
+					{ "html-lsp" },
+					{ "json-lsp" },
+					{ "js-debug-adapter" },
+					{ "lua_ls" },
+					{ "marksman" },
+					{ "node-debug2-adapter" },
+					{ "prettier" },
+					{ "stylua" },
+					{ "tsserver" },
+					{ "tailwindcss-language-server" },
+					{ "vimls" },
+				},
+
+				auto_update = true,
+				run_on_start = true,
+				start_delay = 3000, -- 3 second delay
+				debounce_hours = 5, -- at least 5 hours between attempts to install/update
+			})
+		end,
+	},
 }
