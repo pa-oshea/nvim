1aabf4bf8890621fe1c1f5a11732513e27baca46
Patrick O'Shea <p14oshea@gmail.com>
2024-01-25 14:58:37 +0000

    cmp enter keybind

--- lua/pos/plugins/cmp.lua
+++ lua/pos/plugins/cmp.lua
@@ -43,13 +43,7 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping(function(fallback)
-					if cmp.visible() and cmp.get_active_entry() then
-						cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-					else
-						fallback()
-					end
-				end),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if luasnip.expand_or_locally_jumpable() then
 						luasnip.expand_or_jump()
