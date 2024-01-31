9793d42ee5c83e8bbac16e4ef8e961a4870a8e82
Patrick O'Shea <p14oshea@gmail.com>
2024-01-31 19:45:21 +0000

    cmp fix

--- lua/pos/plugins/cmp.lua
+++ lua/pos/plugins/cmp.lua
@@ -54,7 +54,7 @@ return {
 					s = cmp.mapping.confirm({ select = true }),
 					c = function(fallback)
 						if cmp.visible() and cmp.get_active_entry() then
-							cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+							cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
 						else
 							fallback()
 						end
