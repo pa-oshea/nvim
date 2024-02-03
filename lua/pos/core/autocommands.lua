0c7d91f7f225fc26dc46d0291e8c9eee4884136a
Patrick O'Shea <p14oshea@gmail.com>
2024-02-03 18:35:11 +0000

    Update autocommands to what lazyvim has

--- lua/pos/core/autocommands.lua
+++ lua/pos/core/autocommands.lua
@@ -1,120 +1,107 @@
---- Create autocommand groups based on the passed definitions
---- Also creates the augroup automatically if it doesn't exist
----@param definitions table contains a tuple of event, opts, see `:h nvim_create_autocmd`
-local function define_autocmds(definitions)
-	for _, entry in ipairs(definitions) do
-		local event = entry[1]
-		local opts = entry[2]
-		if type(opts.group) == "string" and opts.group ~= "" then
-			local exists, _ = pcall(vim.api.nvim_get_autocmds, { group = opts.group })
-			if not exists then
-				vim.api.nvim_create_augroup(opts.group, {})
-			end
+-- This file is automatically loaded by lazyvim.config.init.
+
+local function augroup(name)
+	return vim.api.nvim_create_augroup("pos_" .. name, { clear = true })
+end
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+	group = augroup("checktime"),
+	callback = function()
+		if vim.o.buftype ~= "nofile" then
+			vim.cmd("checktime")
 		end
-		vim.api.nvim_create_autocmd(event, opts)
-	end
-end
+	end,
+})
 
-local function load_defaults()
-	local definitions = {
-		{
-			"TextYankPost",
-			{
-				group = "_general_settings",
-				pattern = "*",
-				desc = "Highlight text on yank",
-				callback = function()
-					vim.highlight.on_yank({ higroup = "Search", timeout = 100 })
-				end,
-			},
-		},
-		{
-			"FileType",
-			{
-				group = "_hide_dap_repl",
-				pattern = "dap-repl",
-				command = "set nobuflisted",
-			},
-		},
-		{
-			"FileType",
-			{
-				group = "_filetype_settings",
-				pattern = { "lua" },
-				desc = "fix gf functionality inside .lua files",
-				callback = function()
-					---@diagnostic disable: assign-type-mismatch
-					-- credit: https://github.com/sam4llis/nvim-lua-gf
-					vim.opt_local.include = [[\v<((do|load)file|require|reload)[^''"]*[''"]\zs[^''"]+]]
-					vim.opt_local.includeexpr = "substitute(v:fname,'\\.','/','g')"
-					vim.opt_local.suffixesadd:prepend(".lua")
-					vim.opt_local.suffixesadd:prepend("init.lua")
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = augroup("highlight_yank"),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
 
-					for _, path in pairs(vim.api.nvim_list_runtime_paths()) do
-						vim.opt_local.path:append(path .. "/lua")
-					end
-				end,
-			},
-		},
-		{
-			"FileType",
-			{
-				group = "_buffer_mappings",
-				pattern = {
-					"qf",
-					"help",
-					"man",
-					"floaterm",
-					"lspinfo",
-					"lir",
-					"lsp-installer",
-					"null-ls-info",
-					"tsplayground",
-					"DressingSelect",
-					"Jaq",
-				},
-				callback = function()
-					vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = true })
-					vim.opt_local.buflisted = false
-				end,
-			},
-		},
-		{
-			"VimResized",
-			{
-				group = "_auto_resize",
-				pattern = "*",
-				command = "tabdo wincmd =",
-			},
-		},
-		{
-			"FileType",
-			{
-				group = "_filetype_settings",
-				pattern = "alpha",
-				callback = function()
-					vim.cmd([[
-            nnoremap <silent> <buffer> q :qa<CR>
-            nnoremap <silent> <buffer> <esc> :qa<CR>
-            set nobuflisted
-          ]])
-				end,
-			},
-		},
-		{
-			"FileType",
-			{
-				group = "_filetype_settings",
-				pattern = "lir",
-				callback = function()
-					vim.opt_local.number = false
-					vim.opt_local.relativenumber = false
-				end,
-			},
-		},
-	}
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+	group = augroup("resize_splits"),
+	callback = function()
+		local current_tab = vim.fn.tabpagenr()
+		vim.cmd("tabdo wincmd =")
+		vim.cmd("tabnext " .. current_tab)
+	end,
+})
 
-	define_autocmds(definitions)
-end
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+	group = augroup("last_loc"),
+	callback = function(event)
+		local exclude = { "gitcommit" }
+		local buf = event.buf
+		if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+			return
+		end
+		vim.b[buf].lazyvim_last_loc = true
+		local mark = vim.api.nvim_buf_get_mark(buf, '"')
+		local lcount = vim.api.nvim_buf_line_count(buf)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
 
-load_defaults()
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("close_with_q"),
+	pattern = {
+		"PlenaryTestPopup",
+		"help",
+		"lspinfo",
+		"man",
+		"notify",
+		"qf",
+		"query",
+		"spectre_panel",
+		"startuptime",
+		"tsplayground",
+		"neotest-output",
+		"checkhealth",
+		"neotest-summary",
+		"neotest-output-panel",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("wrap_spell"),
+	pattern = { "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
+
+-- Fix conceallevel for json files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	group = augroup("json_conceal"),
+	pattern = { "json", "jsonc", "json5" },
+	callback = function()
+		vim.opt_local.conceallevel = 0
+	end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = augroup("auto_create_dir"),
+	callback = function(event)
+		if event.match:match("^%w%w+://") then
+			return
+		end
+		local file = vim.loop.fs_realpath(event.match) or event.match
+		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+	end,
+})
