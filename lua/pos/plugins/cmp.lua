9b4c4f932eef1871c954ccec364128ce762d260f
Patrick O'Shea <p14oshea@gmail.com>
2024-02-03 14:54:27 +0000

    cmp complete
    might change again

--- lua/pos/plugins/cmp.lua
+++ lua/pos/plugins/cmp.lua
@@ -41,7 +41,7 @@ return {
 				["<C-k>"] = cmp.mapping.select_prev_item(),
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-l>"] = cmp.mapping.confirm({ select = true }),
 				["<C-e>"] = cmp.mapping.abort(),
 				["<CR>"] = cmp.mapping({
 					i = function(fallback)
